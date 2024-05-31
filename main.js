ef91e661f7a7db45b1857ab51ec7ccd996eff6da
palmtrees_dreamland <palmtrees_dreamland@proton.me>
2024-05-31 17:26:53 +0200

    Add index.html

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let color = "black";
-
+let click = false;
 
 
 document.addEventListener("DOMContentLoaded", function(){
   createBoard(16);
+  document.querySelector("body").addEventListener("click", function(e){
+    if(e.target.tagName != "BUTTON"){
+      click = !click;
+      let draw = document.querySelector("#draw");
+      if(click){
+        draw.innerHTML = "You can now draw";
+        }
+        else {
+          draw.innerHTML = "You cannot draw"
+        }
+      
+    }
+  })
+  
   getSize();
   let btn_popup = document.querySelector("#popup");
   btn_popup.addEventListener("click", function(){
@@ -48,12 +62,14 @@ function getSize(){
 }
 
 function colorDiv(){
-  if(color == "random"){
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-  }
-  else{
-    this.style.backgroundColor = 'black'
-  }
+  if(click){
+    if(color == "random"){
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+      this.style.backgroundColor = 'black'
+    }
+  }  
 }
 
 
